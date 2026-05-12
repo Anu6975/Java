45b58ceb0725456274430ca260986093502b95b6
Anu6975 <badveanu04@gmail.com>
2026-05-12 16:17:10 +0530

    Java Day1

---
+++ src/Day1/basic_program.java
@@ -0,0 +1,46 @@
+package Day1;
+
+public class basic_program {
+
+	public static void main(String[] args) {
+		
+		/* Printing Methods */
+		
+		System.out.println("Hello");
+		System.out.print("My Name is Anuja");
+		System.out.println(" Welcome!!");
+		
+		System.out.println(234+2);
+//		System.out.println(A);
+//		System.out.println(@#$);
+		
+		
+		/* Operators */
+		int a=10, b=5;
+		
+//		1. Arithmetic
+		
+		System.out.println("Addition: "+ (a+b));
+		System.out.println("Subtraction: "+ (a-b));
+		System.out.println("Multiplication: "+ a*b);
+		System.out.println("Division: "+ a/b);
+		System.out.println("Modulus: "+ a%b);
+		
+//		2. Comparison Operators
+		
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		
+//		3. Logical Operators 
+		
+		
+		
+
+		
+	}
+
+}
