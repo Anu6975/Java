29461519df40893b3f1fb8c67ff9c4ca09df1407
Anu6975 <badveanu04@gmail.com>
2026-05-27 16:36:33 +0530

    Update LeapYear.java

--- src/JavaPractical/Day3/LeapYear.java
+++ src/JavaPractical/Day3/LeapYear.java
@@ -1,5 +1,20 @@
 package JavaPractical.Day3;
 
-public class LeapYear {
+import java.util.Scanner;
 
+public class LeapYear {
+	
+	public static void main(String[] a) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Valid Year: ");
+		int year= sc.nextInt();
+		
+		if((year%4==0 && year % 100 !=0 ) || year%400==0) {
+			System.out.println("Entered Year is a Leap year");
+		}
+		else
+		System.out.println("Entered Year is not a Leap year");
+		
+		sc.close();
+	}
 }
